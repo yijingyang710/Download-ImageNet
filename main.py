79fae1cb48f64a88f4f561d4824c7b99618582e6
Yijing Yang <49934758+StephanieYYJ@users.noreply.github.com>
2019-04-23 23:58:47 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,68 @@
+
+from bs4 import BeautifulSoup
+import numpy as np
+import requests
+import cv2
+import urllib
+import matplotlib.pyplot as plt
+import random
+import os
+
+def url_to_image(url):
+# download the image, convert it to a NumPy array, and then read
+# it into OpenCV format
+    resp = urllib.request.urlopen(url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    image = cv2.resize(image,(224,224))
+    # return the image
+    return image
+
+
+def main():        
+    # number of images per class
+    n_of_training_images=10  
+    # read in the wordnet id list
+    infile = open("./wnids1000.txt","r")
+    lines = infile.readlines()
+    wnids = []
+    for line in lines:
+        wnids.append(line.strip('\n').split(' ')[0])
+    # download images
+    for i in range(len(wnids)):
+        print("preparing class #%d"%i)
+        # create sub folders
+        root = "./images/"+str(i)
+        folder = os.path.exists(root)
+        if not folder:
+            os.makedirs(root)
+        # start downloading (BeautifulSoup is an HTML parsing library)
+        url = "http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=" + wnids[i]
+        page = requests.get(url)#ship synset
+        soup = BeautifulSoup(page.content, 'html.parser')#puts the content of the website into the soup variable, each url on a different line
+        str_soup=str(soup) #convert soup to string so it can be split
+        type(str_soup)
+        split_urls=str_soup.split('\r\n')#split so each url is a different possition on a list
+        # shuffle
+        random.shuffle(split_urls)
+        print(len(split_urls))#print the length of the list so you know how many urls you have
+        # store images
+        nn = 0
+        kk = 0
+        while(nn<n_of_training_images):
+            if not split_urls[kk] == None:
+                try:
+                    I = url_to_image(split_urls[kk])
+                    if (len(I.shape))==3: #check if the image has width, length and channels
+    #                    plt.imshow(I)
+    #                    plt.show()
+                        if np.mean(I)<253: # remove failed images (almost all white)
+                            save_path = root+'/img'+str(nn)+'.jpg'#create a name of each image                
+                            cv2.imwrite(save_path,I)
+                            nn=nn+1
+                except:
+                    None
+            kk=kk+1
+
+if __name__ == '__main__':
+    main()
