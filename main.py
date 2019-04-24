6eedfda4117ff4425d77fd397b4e8b6832a688c7
Yijing Yang <49934758+StephanieYYJ@users.noreply.github.com>
2019-04-24 15:53:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 23 14:24:32 2019
+
+@author: yijingyang
+"""
 
 from bs4 import BeautifulSoup
 import numpy as np
@@ -7,19 +14,35 @@ import urllib
 import matplotlib.pyplot as plt
 import random
 import os
+import socket
+#from urllib.request import urlopen
 
 def url_to_image(url):
 # download the image, convert it to a NumPy array, and then read
 # it into OpenCV format
-    resp = urllib.request.urlopen(url)
+#    opener = urllib.request.build_opener()
+#    opener.addheaders = [('User-agent','Mozilla/49.0.2')]
+    try:
+#        op1ener.open(url)
+        resp = urllib.request.urlopen(url,timeout=1)        
+    except urllib.error.HTTPError:
+        print('find one HTTPError')
+        return 0
+    except urllib.errorURLError:
+        print('find one URLError')
+        return 0
+    except socket.timeout:
+        print('time out')
+        return 0
+            
     image = np.asarray(bytearray(resp.read()), dtype="uint8")
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
     image = cv2.resize(image,(224,224))
     # return the image
     return image
 
-
-def main():        
+def main():       
+    startp = 0
     # number of images per class
     n_of_training_images=10  
     # read in the wordnet id list
@@ -28,11 +51,12 @@ def main():
     wnids = []
     for line in lines:
         wnids.append(line.strip('\n').split(' ')[0])
+    wnids = wnids[startp:]
     # download images
     for i in range(len(wnids)):
-        print("preparing class #%d"%i)
+        print("preparing class #%d"%(i+startp))
         # create sub folders
-        root = "./images/"+str(i)
+        root = "./images/"+str(i+startp)
         folder = os.path.exists(root)
         if not folder:
             os.makedirs(root)
@@ -49,20 +73,23 @@ def main():
         # store images
         nn = 0
         kk = 0
-        while(nn<n_of_training_images):
+        while(nn<n_of_training_images and kk<len(split_urls)):
             if not split_urls[kk] == None:
                 try:
                     I = url_to_image(split_urls[kk])
-                    if (len(I.shape))==3: #check if the image has width, length and channels
-    #                    plt.imshow(I)
-    #                    plt.show()
-                        if np.mean(I)<253: # remove failed images (almost all white)
-                            save_path = root+'/img'+str(nn)+'.jpg'#create a name of each image                
-                            cv2.imwrite(save_path,I)
-                            nn=nn+1
+                    if I.any():
+                        if (len(I.shape))==3: #check if the image has width, length and channels
+#                            plt.imshow(I)
+#                            plt.show()
+                            if np.mean(I)<253: # remove failed images (almost all white)
+                                save_path = root+'/img'+str(nn)+'.jpg'#create a name of each image                
+                                cv2.imwrite(save_path,I)
+                                nn=nn+1
                 except:
                     None
             kk=kk+1
+            if kk==len(split_urls):
+                print("not enough valid images!")
 
 if __name__ == '__main__':
     main()
